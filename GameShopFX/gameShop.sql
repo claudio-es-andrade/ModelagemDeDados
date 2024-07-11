85edca3845a8124af1ee048a3cc2a31eda3bf4b6
claudio-es-andrade <claudioe.s.andrade@gmail.com>
2024-07-10 21:15:42 -0300

    Correção de tabelas

--- GameShopFX/gameShop.sql
+++ GameShopFX/gameShop.sql
@@ -4,10 +4,11 @@ create database gameShop;
 
 use gameShop;
 -- Creating Tables
-drop table games;
+-- drop table games;
 -- drop table categories;
 -- drop table clients;
-drop table orders;
+-- drop table orders;
+-- drop table invoices;
 
 create table categories(
 id_category int primary key not null auto_increment,
@@ -34,12 +35,6 @@ constraint fk_id_client foreign key (id_order) references clients(id_client) ON 
 constraint fk_id_game  foreign key (id_order) references categories(id_category) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO games(id_game, name_game, price)
-	VALUES(1, 'COD', '300.45'),
-		  (2, 'NBA FK', '200.55'),
-          (3, 'Among US', '100.95'),
-          (4, 'Sonic', '200.45');
-
 INSERT INTO clients(id_client, name_client)
 	VALUES(1, 'João'),
           (2, 'Maria'),
@@ -53,6 +48,12 @@ INSERT INTO categories(id_category, name_category)
               (3, 'SPORTS'),
               (4, 'FANTASY');
 
+INSERT INTO games(id_game, name_game, price)
+	VALUES(1, 'COD', '300.45'),
+		  (2, 'NBA FK', '200.55'),
+          (3, 'Among US', '100.95'),
+          (4, 'Sonic', '200.45');
+
 
 INSERT INTO orders(id_order , qty)
 		VALUES(1,2),
@@ -60,36 +61,41 @@ INSERT INTO orders(id_order , qty)
               (3, 2),
               (4, 3);
 
-DROP table  games;              
+             
 SELECT * FROM games;
 SELECT * FROM categories;
 SELECT * FROM clients;
 SELECT * FROM orders;
 
-SELECT clients.name_client, orders.qty, games.name_game, games.price * orders.qty AS TOTAL_PRICE, categories.name_category
-FROM orders
-INNER JOIN games ON orders.id_order = games.id_game
-INNER JOIN clients ON orders.id_order = clients.id_client
-INNER JOIN categories ON games.id_game = categories.id_category;
 
 CREATE TABLE invoices(
 	id_invoice int primary key not null auto_increment,
-    qty_invoice int,
     invoice_name varchar(50),
+    qty_invoice int,
     invoice_game_name varchar(50),
     invoice_totalPrice float,
     invoice_category varchar(50)
 );
-DROP TABLE invoices;
-SELECT * FROM invoices;
 
- SELECT clients.id_client, clients.name_client, orders.qty, games.name_game, games.price * orders.qty AS TOTAL_PRICE, categories.name_category
+
+ SELECT clients.id_client, clients.name_client, orders.qty, games.name_game, FORMAT(games.price * orders.qty, 1) AS TOTAL_PRICE, categories.name_category
 FROM orders
 INNER JOIN games ON orders.id_order = games.id_game
 INNER JOIN clients ON orders.id_order = clients.id_client
 INNER JOIN categories ON games.id_game = categories.id_category;
 
+-- Inserting those values above in the invoice table
 
+INSERT INTO invoices (id_invoice, invoice_name, qty_invoice, invoice_game_name, invoice_totalPrice, invoice_category)
+SELECT clients.id_client AS id,
+       clients.name_client AS invoice_name,
+       orders.qty,
+       games.name_game,
+       FORMAT(games.price * orders.qty, 1) AS invoice_totalPrice,
+       categories.name_category
+FROM orders
+INNER JOIN games ON orders.id_order = games.id_game
+INNER JOIN clients ON orders.id_order = clients.id_client
+INNER JOIN categories ON games.id_game = categories.id_category;
 
 SELECT * FROM invoices;
-DROP TABLE invoices;
